234319c98144c8c1e8556f25277cc2ecdd7fa243
Furqan <f.mahmoud@alpha-apps.ae>
2021-06-14 09:18:05 +0300

    api refactoring

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -25,52 +25,56 @@ namespace MoviesApis.Controllers
         [HttpPut]
         public ActionResult<WatchingList> WatchVideo(int movieId, WatchingList item)
         {
+            // validations first 
+            //....
+
+            // Workflow
             // See if the video is already exist in the watching list
             var watchingItem = _movieService.GetByUserAndMoviePin(item.User.Id, item.Video.Id);
-            if (watchingItem != null)
+            switch (item.Video.Type)
             {
-                var percentage = (item.SeekPosition / watchingItem.Video.Duration) * 100;
-                switch (watchingItem.Video.Type)
-                {
-                    case Models.VideoType.Movie:
+                case Models.VideoType.Movie:
+                    var percentage = (item.SeekPosition / item.Video.Duration) * 100;
+                    if (watchingItem != null)
+                    {
                         // Remove the video from the watching list it's crossed 90% of the duration
-                        if (percentage > 90 )
+                        if (percentage > 90)
                         {
                             _movieService.Remove(watchingItem);
                         }
                         else // Otherwise, just update the seek position
                         {
-                            _movieService.Update(watchingItem.Id, item);
+                            watchingItem.SeekPosition = item.SeekPosition;
+                            _movieService.Update(watchingItem.Id, watchingItem);
                         }
-                     break;
-                    case Models.VideoType.Series:
-                        // Remove the series if all the eposides for all the seasons have been watched.
-                        var totalEposidesNumber = _dbContext.Seasons.Where(s => s.SeriesId == watchingItem.Series.Id).Sum(s => s.EposidesNumber);
-                        var seriesWatchedItemsNumber = _movieService.CountWatchedSeriesItems(watchingItem.Series.Id);
-                        if(totalEposidesNumber == seriesWatchedItemsNumber)
+                    }
+                    else // add the record to the watching list if it's not completed already! the movie will not be registered in the continue watching list if the end-user completed it.
+                    {
+                        if (percentage <= 90)
                         {
-                            _movieService.RemoveSeries(watchingItem.Series.Id);
+                            _movieService.Create(item);
                         }
-                        else // Otherwise, just update the seek position
-                        {
-                            item.Id = watchingItem.Id;
-                            _movieService.Update(item.Id, item);
-                        }
-                        break;
-                    default:
-                       
-                     break;
-
-                }
-            }
-            else // add the record to the watching list if it's not completed already!
-            {
-                var video = _dbContext.Videos.Where(v => v.Id == item.Video.Id).SingleOrDefault();
-                var percentage = (item.SeekPosition / video.Duration) * 100;
-                if (percentage <= 90)
-                {
-                    _movieService.Create(item);
-                }
+                    }
+                    break;
+                case Models.VideoType.Series:
+                    
+                    if (watchingItem != null) // just update the seek position
+                    {
+                        watchingItem.SeekPosition = item.SeekPosition;
+                        _movieService.Update(item.Id, item);
+                    } else
+                    {
+                        watchingItem = _movieService.Create(item);
+                    }
+                    // Remove the series if all the eposides for all the seasons have been watched.
+                    var totalEposidesNumber = _dbContext.Seasons.Where(s => s.SeriesId == watchingItem.Series.Id).Sum(s => s.EposidesNumber);
+                    var seriesWatchedItemsNumber = _movieService.CountWatchedSeriesItems(watchingItem.Series.Id);
+                    if (totalEposidesNumber == seriesWatchedItemsNumber)
+                    {
+                        _movieService.RemoveSeries(watchingItem.Series.Id);
+                    }
+                    break;
+                default: break;
             }
             return NoContent();
         }
